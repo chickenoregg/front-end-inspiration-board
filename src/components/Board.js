6dd4e2d262ac23d1fde57be6b3be7c885a162d40
chickenandegg <tei.villanueva@gmail.com>
2023-06-28 12:09:05 -0700

    Add components needed - Board, BoardForm, Card, CardForm, CardList

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardList from './CardList'
+
+function Board() {
+  return (
+      <div>
+          <h1>Hello I am Board</h1>
+          <CardList/>
+      
+    </div>
+  )
+}
+
+export default Board
