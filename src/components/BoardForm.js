80e27a96fed227ba6b27d146f39bc8f792afc887
Sage Choi <coolcuds@gmail.com>
2023-06-29 00:39:32 -0700

    Modify indentation to 2 spaces, add className to form and preview of input title and owner before submitting

--- src/components/BoardForm.js
+++ src/components/BoardForm.js
@@ -7,49 +7,51 @@ const INITIAL_BOARD_DATA = {
 };
 
 const BoardForm = ({ addBoard }) => {
-    const [boardData, setBoardData] = useState(INITIAL_BOARD_DATA);
+  const [boardData, setBoardData] = useState(INITIAL_BOARD_DATA);
 
-    const handleChange = (event) => {
-        const newBoardData = {
-            ...boardData,
-            [event.target.name]: event.target.value
-        };
-        console.log(event.target);
-        setBoardData(newBoardData);
+  const handleChange = (event) => {
+    const newBoardData = {
+      ...boardData,
+      [event.target.name]: event.target.value
     };
+    console.log(event.target);
+    setBoardData(newBoardData);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addBoard(boardData);
-        setBoardData(INITIAL_BOARD_DATA);
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addBoard(boardData);
+    setBoardData(INITIAL_BOARD_DATA);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="title">Title</label>
-            <input
-                required
-                type="text"
-                id="title"
-                name="title"
-                value={boardData.title}
-                onChange={handleChange}
-            />
-            <label htmlFor="owner">Owner Name</label>
-            <input
-                required
-                type="text"
-                id="owner"
-                name="owner"
-                value={boardData.owner}
-                onChange={handleChange}
-            />
-            <input type="submit" value="submit"/>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit} className='new-board-form__form'>
+      <label htmlFor="title">Title</label>
+      <input
+        required
+        type="text"
+        id="title"
+        name="title"
+        value={boardData.title}
+        onChange={handleChange}
+      />
+      <label htmlFor="owner">Owner Name</label>
+      <input
+        required
+        type="text"
+        id="owner"
+        name="owner"
+        value={boardData.owner}
+        onChange={handleChange}
+      />
+      <input type="submit" value="submit"/>
+      <p>Preview: {boardData.title} - {boardData.owner}</p>
+    </form>
+  );
 };
 
 BoardForm.propTypes = {
-    addBoard: PropTypes.func.isRequired,
+  addBoard: PropTypes.func.isRequired
 };
+
 export default BoardForm;
