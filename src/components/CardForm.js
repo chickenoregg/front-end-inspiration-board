c988755a32b68865b983894dcc54910d699536a8
Sage Choi <coolcuds@gmail.com>
2023-06-29 00:26:22 -0700

    Modify indentation to 2 spaces

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -6,40 +6,41 @@ const INITIAL_CARD_DATA = {
 };
 
 const CardForm = ({ addCard }) => {
-    const [cardData, setCardData] = useState(INITIAL_CARD_DATA);
+  const [cardData, setCardData] = useState(INITIAL_CARD_DATA);
 
-    const handleChange = (event) => {
-        const newCardData = {
-            ...cardData,
-            [event.target.name]: event.target.value
-        };
-        console.log(event.target);
-        setCardData(newCardData);
+  const handleChange = (event) => {
+    const newCardData = {
+      ...cardData,
+      [event.target.name]: event.target.value
     };
+    console.log(event.target);
+    setCardData(newCardData);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addCard(cardData);
-        setCardData(INITIAL_CARD_DATA);
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addCard(cardData);
+    setCardData(INITIAL_CARD_DATA);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="message">Message</label>
-            <input
-                required
-                type="text"
-                id="message"
-                name="message"
-                value={cardData.message}
-                onChange={handleChange}
-            />
-            <input type="submit" value="submit"/>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="message">Message</label>
+      <input
+        required
+        type="text"
+        id="message"
+        name="message"
+        value={cardData.message}
+        onChange={handleChange}
+      />
+      <input type="submit" value="submit" />
+    </form>
+  )
 };
 
 CardForm.propTypes = {
-    addCard: PropTypes.func.isRequired,
+  addCard: PropTypes.func.isRequired,
 };
+
 export default CardForm;
