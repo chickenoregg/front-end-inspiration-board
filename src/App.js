d1ecf39348f51a37df374003f85611a297484580
Sage Choi <coolcuds@gmail.com>
2023-06-28 18:12:00 -0700

    Add state changing hooks, method to retrieve boards, set a selected board, and add components to JSX

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Board from './components/Board';
+import BoardList from './components/BoardList';
 import CardList from './components/CardList';
 
+
+const DEFAULT_BOARD_DATA = {
+  board_id: null,
+  owner: '',
+  title: ''
+}
+
+
 function App() {
+  const [boardsData, setBoardsData] = useState([]);
+  const [selectedBoard, setSelectedBoard] = useState(DEFAULT_BOARD_DATA);
+
+  // Retrieve boards using API call
+  const getBoards = () => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {})
+      .then((response) => {
+        setBoardsData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Unable to retrieve boards.');
+      });
+  };
+
+  useEffect(() => {
+    getBoards();
+  }, []);
+
+  const selectBoard = (board) => {
+    setSelectedBoard(board);
+  }
+
   return (
     <div className="App">
       <main>
         <header>Inspiration Board</header>
-        <Board></Board>
+        <section>
+          <h2>Boards</h2>
+          <BoardList
+            boards={boardsData}
+            onBoardSelect={selectBoard}
+          />
+        </section>
+        <section>
+          <h2>Selected Board</h2>
+          <p>Select a board from the board list!</p>
+        </section>
         <CardList/>
       </main>
-
     </div>
   );
 }
