85884056ec595f100459d4d58bb5d2f4433febb4
Sage Choi <coolcuds@gmail.com>
2023-06-28 19:55:59 -0700

    Clean up comments and spacing, and add function to create a new board

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ const DEFAULT_BOARD_DATA = {
   board_id: null,
   owner: '',
   title: ''
-}
-
+};
 
 function App() {
   const [boardsData, setBoardsData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState(DEFAULT_BOARD_DATA);
 
-  // Retrieve boards using API call
   const getBoards = () => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {})
@@ -24,7 +22,7 @@ function App() {
         setBoardsData(response.data);
       })
       .catch((error) => {
-        console.log(error);
+        console.log('Error:', error);
         alert('Unable to retrieve boards.');
       });
   };
@@ -37,22 +35,43 @@ function App() {
     setSelectedBoard(board);
   }
 
+  const createNewBoard = (newBoard) => {
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/boards`)
+      .then((response) => {
+        const boards = [...boardsData];
+        boards.push(response.data.board);
+        setBoardsData(boards);
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+        alert('Unable to create a new board.');
+      });
+  };
+
   return (
     <div className="App">
       <main>
         <header>Inspiration Board</header>
-        <section>
-          <h2>Boards</h2>
-          <BoardList
-            boards={boardsData}
-            onBoardSelect={selectBoard}
-          />
-        </section>
-        <section>
-          <h2>Selected Board</h2>
-          <p>
-            {selectedBoard.board_id ? `${selectedBoard.title} - ${selectedBoard.owner}` : 'Select a board from the board list!'}
-          </p>
+        <section className='boards__container'>
+          <section>
+            <h2>Boards</h2>
+            <ol className='boards__list'>
+              <BoardList
+                boards={boardsData}
+                onBoardSelect={selectBoard}
+              />
+            </ol>
+          </section>
+          <section>
+            <h2>Selected Board</h2>
+            <p>
+              {selectedBoard.board_id ? `${selectedBoard.title} - ${selectedBoard.owner}` : 'Select a board from the board list!'}
+            </p>
+          </section>
+          <section>
+            <h2>Create a New Board</h2>
+          </section>
         </section>
         {selectedBoard.board_id ? <CardList selectedBoardId={selectedBoard.board_id} /> : ''}
       </main>
