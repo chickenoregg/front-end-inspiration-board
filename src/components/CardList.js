db8a4ea5e263237f8694d6190e1b0996fe484507
Sage Choi <coolcuds@gmail.com>
2023-06-28 18:24:07 -0700

    Modify indentation and layout of Card component call

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,32 +4,34 @@ import Card from './Card'
 
 const CardList = ({ selectedBoardId }) => {
 
-    const [cardsData, setCardsData] = useState([]);
+  const [cardsData, setCardsData] = useState([]);
 
-    useEffect(() => {
-        if (selectedBoardId) {
-            axios
-              .get(
-                `${process.env.REACT_APP_BACKEND_URL}/boards/${selectedBoardId}/cards`
-              )
-              .then((response) => {
-                setCardsData(response);
-              })
-              .catch((error) => {
-                console.log("Error:", error);
-              });
-        }
-    }, [selectedBoardId]);
+  useEffect(() => {
+    if (selectedBoardId) {
+      axios
+        .get(
+          `${process.env.REACT_APP_BACKEND_URL}/boards/${selectedBoardId}/cards`
+        )
+        .then((response) => {
+          setCardsData(response);
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+        });
+    }
+  }, [selectedBoardId]);
 
-    return (
-        <div>
-            <h1>CardList</h1>
-            {cardsData.map((card) => (
-                <Card key={card.id}
-                    card={card} />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <h1>CardList</h1>
+      {cardsData.map((card) => (
+        <Card
+          key={card.id}
+          card={card}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CardList;
