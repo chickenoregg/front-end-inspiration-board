0661f685897a3c45cd2a2c8eb4b902b092c4968b
Sage Choi <coolcuds@gmail.com>
2023-06-29 01:17:53 -0700

    Add styling for each card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const Card = ({ card }) => {
-      return (
-        <div>
-          <h1>{card.message}</h1>
-          <h2>{card.likes_count}</h2>
-        </div>
-      );
-}
+const Card = ({ card, increaseCardLikes, deleteCard }) => {
+  return (
+    <div className='card-item'>
+      <p>{card.message}</p>
+      <ul className='card-item__controls'>
+        <li>{card.likes_count} 👍</li>
+        <li onClick={() => increaseCardLikes(card)}>+1 👍</li>
+        <li className='card-item__delete' onClick={() => deleteCard(card)}>Delete</li>
+      </ul>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  card: PropTypes.object.isRequired,
+  increaseCardLikes: PropTypes.func.isRequired,
+  deleteCard: PropTypes.func.isRequired
+};
 
 export default Card;
